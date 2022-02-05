13eec1fccf6ed408d3f9f35df3e52f2686389dfd
Alexantoun <oz.antoun@gmail.com>
2022-02-05 16:06:57 -0700

    Started: Adding two numbers challenge

---
+++ AddTwoNumber.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int operator [] (listNode*, int)
+    {
+        
+    }
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        cout<<l1.val<<endl;
+        
+    }
+};

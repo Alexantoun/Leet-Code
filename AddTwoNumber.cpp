588bb4466f0bbb33bf0de967750efc687f6e107e
Alexantoun <oz.antoun@gmail.com>
2022-02-08 15:44:47 -0700

    making progress

--- AddTwoNumber.cpp
+++ AddTwoNumber.cpp
@@ -10,22 +10,32 @@
  */
 class Solution {
 public:
-    ListNode* indexing(ListNode* node2Find, int i){
-        int count = 0;
-        while(count < i)
-        {
-            node2Find = node2Find->next;
-            count++;
-        }
-        return node2Find;
-    }
-    
+        
 //Below is main code
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode* l3 = new ListNode;
-        ListNode* target = NULL;
+        ListNode* l2Target = l2, *l1Target = l1, *l3Target = l3;
+        int excess = 0;
         
-        cout<<target->val<<endl;
+        while(l1Target != NULL || l2Target!=NULL)
+        {
+            l3Target->val += l1Target->val + l2Target->val + excess;
+            excess = 0;
+            while (l3Target->val >= 10){
+                excess++;
+                l3Target->val -= 10;
+            }
+            l3Target->next = new ListNode;
+            l3Target = l3Target->next;  
+            l1Target = l1Target->next;
+            l2Target = l2Target->next;
+        }
+        l3Target = l3;
+        while(l3Target->next != NULL)
+        {
+            cout<<l3Target->val<<endl;
+            l3Target = l3Target->next;
+        }
         
         return l1;
     }

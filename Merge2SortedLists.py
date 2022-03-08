42ce9da5b9387c01cda8f1a903047d90e542e4ce
Alexantoun <oz.antoun@gmail.com>
2022-03-07 23:06:47 -0700

    Finished the merge two sorted lists challenge

--- Merge2SortedLists.py
+++ Merge2SortedLists.py
@@ -6,31 +6,36 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         list3 = ListNode()
-        headValue = list3
-        if list1.val <= list2.val:
-            list3.val = list1.val
-            list1 = list1.next
-        else:
-            list3.val = list2.val
-            list2 = list2.next
-            
-        while(list1 != None and list2 != None):
-            list3.next = ListNode
-            list3 = list3.next
-            if list1.val <= list2.val:
-                list3.val = list1.val
-                list1 = list1.next
-            elif list2.val < list1.val:
-                list3.val = list2.val
-                list2 = list2.next
+        head = list3
+
+        if list1 == None and list2 == None:
+            return None
         
-            
-        list3 = headValue
-        while(list3.next != None):
-            print(list3.val, ", ", end="")
-            list3=list3.next
-        return headValue
-            
+        while(list1!=None or list2!=None):
+            if list1!=None and list2!=None:
+                if list1.val <= list2.val:
+                    list3.val = list1.val
+                    list1=list1.next  
+                else:
+                    list3.val=list2.val
+                    list2=list2.next
+            elif list1!=None and list2==None:
+                list3.val = list1.val
+                list1=list1.next
+            elif list2!=None and list1==None:
+                list3.val = list2.val
+                list2=list2.next
+            else:
+                list3 = None
+            list3.next=ListNode()
+            list3 = list3.next
+        
+        list3 = head
+        while list3.next.next != None:
+            list3 = list3.next
+        list3.next = None    
+        
+        return head
                 
         
         
